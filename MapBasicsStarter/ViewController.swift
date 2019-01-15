cbb1df82d62948fad3493a766e12a7dc19895821
Tim Ng <timothyng2008@me.com>
2019-01-15 19:10:59 +0800

    Initial commit

---
+++ MapBasicsStarter/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MapBasicsStarter
+//
+//  Created by Tim Ng on 1/15/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//------------------------------------------
+    // MARK: - Properties
+    // 1. Add MKMapView
+    // 2. Add UILongPressureGestureRecognizer to drop pin
+    // 3. Add annotations data source
+    
+    
+//------------------------------------------
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1. Add subviews
+        // 2. NSLayoutConstraints activation
+        
+    }
+    
+//------------------------------------------
+    // MARK: - Actions
+    // 1. Add handleLongPress
+    
+//------------------------------------------
+    // MARK: - MapView Delegate Methods
+    // 1. Add mapView(_ mapView:MKMapView, didSelect view: MKAnnotationView)
+    
+
+
+}
+
